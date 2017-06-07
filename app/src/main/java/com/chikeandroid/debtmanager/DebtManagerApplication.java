ed00d20baf7eb3dfa94e5b1007b62610cd7f8bd4
Chike <chikespott@gmail.com>
2017-06-07 22:44:50 +0100

    .gitignore is now working

--- app/src/main/java/com/chikeandroid/debtmanager/DebtManagerApplication.java
+++ app/src/main/java/com/chikeandroid/debtmanager/DebtManagerApplication.java
@@ -4,8 +4,6 @@ import android.app.Application;
 import android.os.StrictMode;
 
 import com.facebook.stetho.Stetho;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 
 /**
@@ -20,7 +18,6 @@ public class DebtManagerApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
 
         // no need to include the applicationModule since it requires no constructor argument
         mApplicationComponent = DaggerApplicationComponent.builder().context(getApplicationContext()).build();
